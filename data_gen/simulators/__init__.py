ced8358b38b0be7fa81504edb475e76782c5ac87
pablomerc <pablomer@mit.edu>
2025-11-23 23:38:25 +0000

    Added an option for two_body (still nede to implement that simulation tho)

--- data_gen/simulators/__init__.py
+++ data_gen/simulators/__init__.py
@@ -5,8 +5,11 @@ def get_simulator(config):
 
   elif config.simulator == 'double_pendulum':
     from .double_pendulum import Simulator
-  
+
+  elif config.simulator == 'two_body':
+    from .two_body import Simulator
+
   else:
     raise NotImplementedError('Simulator not implemented.')
-  
+
   return Simulator(config)
