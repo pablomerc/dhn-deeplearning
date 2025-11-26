00d4889666fa4399d1b496cf89b017c129f52378
pablomerc <pablomer@mit.edu>
2025-11-26 02:01:42 +0000

    I dont think i changed anything lol

--- data_gen/configs/two_body.py
+++ data_gen/configs/two_body.py
@@ -10,13 +10,13 @@ def get_config():
   config.num_data = 1000
   config.num_vis = 5
 
-  config.t_span = (0, 500)
+  config.t_span = (0, 10)
   config.num_timesteps = 1025
 
   config.constants = constants = ml_collections.ConfigDict()
   constants.G = 1.0  # Gravitational constant (can be normalized to 1.0)
   constants.m1 = (0.5, 2.0)  # Mass of body 1 [kg]
   constants.m2 = (0.5, 2.0)  # Mass of body 2 [kg]
-  constants.r1 = (1,5) # Orbital radius of body 1 [km]
-  constants.r2 = (6,10) # Orbital radius of body 2 [km]
+  constants.r1 = (0.5,1.0) # Orbital radius of body 1 [km]
+  constants.r2 = (1.1,1.5) # Orbital radius of body 2 [km]
   return config
